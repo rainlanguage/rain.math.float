fc967067ac191f5165abe66de68f55f591009695
thedavidmeister <thedavidmeister@gmail.com>
2025-04-07 12:40:54 +0400

    format

---
+++ src/lib/format/LibFormatDecimalFloat.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 thedavidmeister
+pragma solidity ^0.8.25;
+
+import {LibDecimalFloat, Float} from "../LibDecimalFloat.sol";
+
+import {LibFixedPointDecimalFormat} from "rain.math.fixedpoint/lib/format/LibFixedPointDecimalFormat.sol";
+
+library LibFormatDecimalFloat {
+    /// Format a decimal float as a string.
+    /// Currently is a thin wrapper around converting to a fixed point decimal
+    /// and then formatting that as a string.
+    /// In the future this may be extended to support a wider range of possible
+    /// values.
+    /// @param signedCoefficient The signed coefficient of the decimal float.
+    /// @param exponent The exponent of the decimal float.
+    /// @return The string representation of the decimal float.
+    function toDecimalString(int256 signedCoefficient, int256 exponent) internal pure returns (string memory) {
+        uint256 decimal18Value = LibDecimalFloat.toFixedDecimalLossless(signedCoefficient, exponent, 18);
+        return LibFixedPointDecimalFormat.fixedPointToDecimalString(decimal18Value);
+    }
+
+    function toDecimalString(Float memory float) internal pure returns (string memory) {
+        return toDecimalString(float.signedCoefficient, float.exponent);
+    }
+}
---
+++ test/src/lib/format/LibFormatDecimalFloat.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: LicenseRef-DCL-1.0
+// SPDX-FileCopyrightText: Copyright (c) 2020 thedavidmeister
+pragma solidity ^0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+import {Float, LibDecimalFloat} from "src/lib/LibDecimalFloat.sol";
+import {LibFormatDecimalFloat} from "src/lib/format/LibFormatDecimalFloat.sol";
+import {LibParseDecimalFloat} from "src/lib/parse/LibParseDecimalFloat.sol";
+
+contract LibFormatDecimalFloatTest is Test {
+    using LibDecimalFloat for Float;
+
+    function toDecimalStringExternal(int256 signedCoefficient, int256 exponent) external pure returns (string memory) {
+        return LibFormatDecimalFloat.toDecimalString(signedCoefficient, exponent);
+    }
+
+    function toString(Float memory float) external pure returns (string memory) {
+        return LibFormatDecimalFloat.toDecimalString(float);
+    }
+
+    /// Check that the memory version matches the stack version.
+    function testFormatMem(Float memory float) external {
+        try this.toDecimalStringExternal(float.signedCoefficient, float.exponent) returns (string memory formatted) {
+            string memory actual = this.toString(float);
+            assertEq(formatted, actual, "Formatted value mismatch");
+        } catch (bytes memory err) {
+            vm.expectRevert(err);
+            LibFormatDecimalFloat.toDecimalString(float);
+        }
+    }
+
+    /// Test round tripping a value through parse and format.
+    function testRoundTrip(uint256 value) external pure {
+        value = bound(value, 0, type(uint256).max / 10);
+        Float memory float = LibDecimalFloat.fromFixedDecimalLosslessMem(value, 18);
+        string memory formatted = LibFormatDecimalFloat.toDecimalString(float);
+        (bytes4 errorCode, Float memory parsed) = LibParseDecimalFloat.parseDecimalFloat(formatted);
+        assertEq(errorCode, 0, "Parse error");
+        assertTrue(float.eq(parsed), "Round trip failed");
+    }
+}
