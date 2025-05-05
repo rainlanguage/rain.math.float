e41208d63b1ab0439c8d770659408f8664c92d95
thedavidmeister <thedavidmeister@gmail.com>
2025-05-05 11:27:31 +0400

    simplify float

--- src/lib/format/LibFormatDecimalFloat.sol
+++ src/lib/format/LibFormatDecimalFloat.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.25;
 import {LibDecimalFloat, Float} from "../LibDecimalFloat.sol";
 
 import {LibFixedPointDecimalFormat} from "rain.math.fixedpoint/lib/format/LibFixedPointDecimalFormat.sol";
+import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 library LibFormatDecimalFloat {
     /// Format a decimal float as a string.
@@ -12,10 +13,10 @@ library LibFormatDecimalFloat {
     /// and then formatting that as a string.
     /// In the future this may be extended to support a wider range of possible
     /// values.
-    /// @param signedCoefficient The signed coefficient of the decimal float.
-    /// @param exponent The exponent of the decimal float.
+    /// @param float The decimal float to format.
     /// @return The string representation of the decimal float.
-    function toDecimalString(int256 signedCoefficient, int256 exponent) internal pure returns (string memory) {
+    function toDecimalString(Float float) internal pure returns (string memory) {
+        (int256 signedCoefficient, int256 exponent) = LibDecimalFloat.unpack(float);
         string memory prefix = "";
         if (signedCoefficient < 0) {
             prefix = "-";
@@ -23,10 +24,5 @@ library LibFormatDecimalFloat {
         }
         uint256 decimal18Value = LibDecimalFloat.toFixedDecimalLossless(signedCoefficient, exponent, 18);
         return string.concat(prefix, LibFixedPointDecimalFormat.fixedPointToDecimalString(decimal18Value));
-    }
-
-    function toDecimalString(Float float) internal pure returns (string memory) {
-        (int256 signedCoefficient, int256 exponent) = LibDecimalFloat.unpack(float);
-        return toDecimalString(signedCoefficient, exponent);
     }
 }
