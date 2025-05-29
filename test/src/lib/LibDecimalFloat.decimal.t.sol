dd4bae72f279d606a07d89167f988a9a28b5d2c0
thedavidmeister <thedavidmeister@gmail.com>
2025-05-29 23:21:03 +0400

    fmt

--- test/src/lib/LibDecimalFloat.decimal.t.sol
+++ test/src/lib/LibDecimalFloat.decimal.t.sol
@@ -313,7 +313,8 @@ contract LibDecimalFloatDecimalTest is Test {
             vm.assume(c / scale != unsignedCoefficient);
         }
         vm.expectRevert(stdError.arithmeticError);
-        (uint256 value, bool lossless) = this.toFixedDecimalLossyExternal(signedCoefficient, exponent, unsignedCoefficient * scale);
+        (uint256 value, bool lossless) =
+            this.toFixedDecimalLossyExternal(signedCoefficient, exponent, unsignedCoefficient * scale);
         (value, lossless);
     }
 }
