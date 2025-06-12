8800872fd912d4917d650802761dfbff30480c5e
brusher_ru <brusher.designer@gmail.com>
2025-06-12 15:17:02 -0300

    test: add tetsts for `concrete/parse`

--- test/src/concrete/DecimalFloat.parse.t.sol
+++ test/src/concrete/DecimalFloat.parse.t.sol
@@ -26,4 +26,52 @@ contract DecimalFloatParseTest is Test {
             deployed.parse(str);
         }
     }
+
+    function checkParseExternal(
+        string memory str,
+        Float expectedFloat
+    ) internal pure {
+        (bytes4 errorSelector, Float float) = LibParseDecimalFloat.parseDecimalFloat(str);
+
+        assertEq(errorSelector, bytes4(0));
+        assertEq(Float.unwrap(float), Float.unwrap(expectedFloat));
+    }
+
+    // Helper function
+    function F(uint256 x) internal pure returns (Float) {
+        return Float.wrap(bytes32(x));
+    }
+
+    function testParseExternalIntegers() external pure {
+        checkParseExternal("0", F(0x00));
+        checkParseExternal("1", F(0x01));
+        checkParseExternal("10", F(0x0A));
+        checkParseExternal("100", F(0x64));
+        checkParseExternal("1000", F(0x03E8));
+        checkParseExternal("2", F(0x02));
+    }
+
+    function testParseExternalDecimals() external pure {
+        checkParseExternal("1.0", F(0x01));
+        checkParseExternal("0.1", F(0xffffffff00000000000000000000000000000000000000000000000000000001));
+        checkParseExternal("0.01", F(0xfffffffe00000000000000000000000000000000000000000000000000000001));
+        checkParseExternal("0.001", F(0xfffffffd00000000000000000000000000000000000000000000000000000001));
+        checkParseExternal("0.0002", F(0xfffffffc00000000000000000000000000000000000000000000000000000002));
+        checkParseExternal("1.5931", F(0xfffffffc00000000000000000000000000000000000000000000000000003e3b));
+    }
+
+    function testParseExternalNegativeIntegers() external pure {
+        checkParseExternal("-0", F(0x00));
+        checkParseExternal("-1", F(0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffff));
+        checkParseExternal("-2", F(0x00000000fffffffffffffffffffffffffffffffffffffffffffffffffffffffe));
+        checkParseExternal("-10", F(0x00000000fffffffffffffffffffffffffffffffffffffffffffffffffffffff6));
+        checkParseExternal("-100", F(0x00000000ffffffffffffffffffffffffffffffffffffffffffffffffffffff9c));
+    }
+
+
+    function testParseExternalNegativeDecimals() external pure {
+        checkParseExternal("-0.01", F(0xfffffffeffffffffffffffffffffffffffffffffffffffffffffffffffffffff));
+        checkParseExternal("-0.001", F(0xfffffffdffffffffffffffffffffffffffffffffffffffffffffffffffffffff));
+        checkParseExternal("-0.0003", F(0xfffffffcfffffffffffffffffffffffffffffffffffffffffffffffffffffffd));
+    }
 }
