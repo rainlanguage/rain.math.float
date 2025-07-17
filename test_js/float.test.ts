ea8fb9add362a4bb507cf4acea89f0d7afbb3ace
rouzwelt <rouzweltt@gmail.com>
2025-07-17 19:15:15 +0000

    Update float.test.ts

--- test_js/float.test.ts
+++ test_js/float.test.ts
@@ -11,10 +11,10 @@ describe('Test Float Bindings', () => {
 
 	async function runTests(path: 'cjs' | 'esm') {
 		// load the Float class from the appropriate module
-		const Float = await (async () => {
+		const { Float } = await (async () => {
 			return path === 'cjs'
-				? (await import('../dist/cjs')).Float
-				: (await import('../dist/esm')).Float;
+				? import('../dist/cjs')
+				: import('../dist/esm');
 		})();
 
 		it('should test parse', () => {
