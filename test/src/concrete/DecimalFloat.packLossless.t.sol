7e9628be0629bc5d27d351e3c703d3a83d15bd7a
thedavidmeister <thedavidmeister@gmail.com>
2025-07-24 16:40:42 +0400

    packlossless test

---
+++ test/src/concrete/DecimalFloat.packLossless.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+import {DecimalFloat} from "src/concrete/DecimalFloat.sol";
+import {LibDecimalFloat, Float} from "src/lib/LibDecimalFloat.sol";
+
+contract DecimalFloatPackLosslessTest is Test {
+    function packLosslessExternal(int224 signedCoefficient, int32 exponent) external pure returns (Float) {
+        return LibDecimalFloat.packLossless(signedCoefficient, exponent);
+    }
+
+    function testPackDeployed(int224 signedCoefficient, int32 exponent) external {
+        DecimalFloat deployed = new DecimalFloat();
+
+        try this.packLosslessExternal(signedCoefficient, exponent) returns (Float packed) {
+            Float deployedPacked = deployed.packLossless(signedCoefficient, exponent);
+
+            assertEq(Float.unwrap(packed), Float.unwrap(deployedPacked));
+        } catch (bytes memory err) {
+            vm.expectRevert(err);
+            deployed.packLossless(signedCoefficient, exponent);
+        }
+    }
+}
