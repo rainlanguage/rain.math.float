abfe62cf6313de6693e47356a5d56fb88bcae7de
thedavidmeister <thedavidmeister@gmail.com>
2025-07-24 17:19:10 +0400

    fix tests

---
+++ test/src/concrete/DecimalFloat.toFixedDecimalLossless.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: CAL
+pragma solidity =0.8.25;
+
+import {Test} from "forge-std/Test.sol";
+import {DecimalFloat} from "src/concrete/DecimalFloat.sol";
+import {LibDecimalFloat, Float} from "src/lib/LibDecimalFloat.sol";
+
+contract DecimalFloatToFixedDecimalLosslessTest is Test {
+    using LibDecimalFloat for Float;
+
+    function toFixedDecimalLosslessExternal(Float packed, uint8 decimals) external pure returns (uint256) {
+        return LibDecimalFloat.toFixedDecimalLossless(packed, decimals);
+    }
+
+    function testToFixedDecimalLosslessDeployed(Float packed, uint8 decimals) external {
+        DecimalFloat deployed = new DecimalFloat();
+
+        try this.toFixedDecimalLosslessExternal(packed, decimals) returns (uint256 fixedDecimal) {
+            uint256 deployedFixedDecimal = deployed.toFixedDecimalLossless(packed, decimals);
+
+            assertEq(fixedDecimal, deployedFixedDecimal);
+        } catch (bytes memory err) {
+            vm.expectRevert(err);
+            deployed.toFixedDecimalLossless(packed, decimals);
+        }
+    }
+}
