3aa059007dfa5e01caf510dce7a728cc4895cca6
thedavidmeister <thedavidmeister@gmail.com>
2025-08-18 18:39:09 +0400

    fmt

--- crates/float/src/lib.rs
+++ crates/float/src/lib.rs
@@ -1370,7 +1370,10 @@ mod tests {
 
         dbg!(&err);
 
-        assert!(matches!(err, FloatError::DecimalFloat(DecimalFloatErrors::MulDivOverflow(_))));
+        assert!(matches!(
+            err,
+            FloatError::DecimalFloat(DecimalFloatErrors::MulDivOverflow(_))
+        ));
     }
 
     #[test]
