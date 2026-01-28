44e0ac429b340466c81f02be729a1aea5fc8d4fb
thedavidmeister <thedavidmeister@gmail.com>
2026-01-28 19:10:05 +0400

    test deployments for decimalfloat

--- src/lib/deploy/LibDecimalFloatDeploy.sol
+++ src/lib/deploy/LibDecimalFloatDeploy.sol
@@ -16,11 +16,21 @@ import {DecimalFloat} from "../../concrete/DecimalFloat.sol";
 import {LOG_TABLE_DISAMBIGUATOR} from "../table/LibLogTable.sol";
 import {WriteError} from "../../error/ErrDecimalFloat.sol";
 
-/// @dev Zoltu deterministic deployment proxy address.
-/// https://github.com/Zoltu/deterministic-deployment-proxy?tab=readme-ov-file#proxy-address
-address constant ZOLTU_PROXY_ADDRESS = 0x7A0D94F55792C434d74a40883C6ed8545E406D12;
-
 library LibDecimalFloatDeploy {
+    /// @dev Zoltu deterministic deployment proxy address.
+    /// https://github.com/Zoltu/deterministic-deployment-proxy?tab=readme-ov-file#proxy-address
+    address constant ZOLTU_PROXY_ADDRESS = 0x7A0D94F55792C434d74a40883C6ed8545E406D12;
+
+    /// @dev Address of the DecimalFloat contract deployed via Zoltu's
+    /// deterministic deployment proxy.
+    /// This address is the same across all EVM-compatible networks.
+    address constant ZOLTU_DEPLOYED_DECIMAL_FLOAT_ADDRESS = address(0x6421E8a23cdEe2E6E579b2cDebc8C2A514843593);
+
+    /// @dev The expected codehash of the DecimalFloat contract deployed via
+    /// Zoltu's deterministic deployment proxy.
+    bytes32 constant DECIMAL_FLOAT_DATA_CONTRACT_HASH =
+        0x2573004ac3a9ee7fc8d73654d76386f1b6b99e34cdf86a689c4691e47143420f;
+
     function combinedTables() internal pure returns (bytes memory) {
         return abi.encodePacked(
             LOG_TABLES,
@@ -39,6 +49,12 @@ library LibDecimalFloatDeploy {
         return container;
     }
 
+    /// Deploys a DecimalFloat contract using Zoltu's deterministic deployment
+    /// proxy contract. This allows the concrete DecimalFloat contract to be
+    /// found at a predictable location regardless of the network.
+    /// Reverts with WriteError if deployment fails.
+    /// @return deployedAddress The address of the deployed DecimalFloat
+    /// contract.
     function decimalFloatZoltu() internal returns (DecimalFloat deployedAddress) {
         //slither-disable-next-line too-many-digits
         bytes memory code = type(DecimalFloat).creationCode;
